e1030ce0533478c273b2937b82f17d0961a9611b
shoxrux <shoxrux@example.com>
2026-07-18 12:00:56 +0500

    Fix transactions notification dependency bootstrap

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -3,10 +3,15 @@ import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SquadsModule } from '../squads/squads.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => SquadsModule)],
+  imports: [
+    PrismaModule,
+    forwardRef(() => NotificationsModule),
+    forwardRef(() => SquadsModule),
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -58,6 +58,7 @@ export class TransactionsService {
 
   constructor(
     private prisma: PrismaService,
+    @Inject(forwardRef(() => NotificationsService))
     private notificationsService: NotificationsService,
     @Inject(forwardRef(() => SquadsService))
     private squadsService: SquadsService,
